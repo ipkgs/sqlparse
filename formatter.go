0527e0f67572360d80f50e1d98ffb7c28e20de27
Sergio Moura <sergio@moura.ca>
2024-04-19 10:30:40 -0400

    tidy up formatter

--- formatter.go
+++ formatter.go
@@ -16,60 +16,136 @@ func FormatOptionFromBreakCount(value int) FormatOption {
 		f.fromBreakCount = value
 	}
 }
+func FormatOptionRemoveComments(value bool) FormatOption {
+	return func(f *formatOptionList) {
+		f.removeComments = value
+	}
+}
 
 type formatOptionList struct {
 	reident        bool
 	fromBreakCount int
+	removeComments bool
+
+	parenthesisIdented []bool
+	writtenInThisLine  bool
+	lastWrittenToken   *Token
+	spaceQueued        string
+	buf                strings.Builder
 }
 
-func Format(tokens []Token, optionList ...FormatOption) string {
-	var options formatOptionList
-
-	for _, option := range optionList {
-		option(&options)
-	}
-
-	var sb strings.Builder
-	var parenthesisIdented []bool
-
-	var writtenInThisLine bool
-	write := func(s string) {
-		if options.reident {
-			if s == "\n" {
-				writtenInThisLine = false
-			} else if !writtenInThisLine {
-				var identLevel int
-				for _, b := range parenthesisIdented {
-					if b {
-						identLevel++
-					}
+func (f *formatOptionList) write(s string) {
+	if f.reident {
+		if trimmedSpace := strings.Trim(s, " \t"); trimmedSpace == "" {
+			f.spaceQueued += s
+			return
+		}
+		if s == "\n" {
+			f.spaceQueued = ""
+			f.writtenInThisLine = false
+		} else if !f.writtenInThisLine {
+			var identLevel int
+			for _, b := range f.parenthesisIdented {
+				if b {
+					identLevel++
 				}
-
-				sb.WriteString(strings.Repeat("  ", identLevel))
-				writtenInThisLine = true
 			}
-		}
-		sb.WriteString(s)
-	}
-	writeLinebreak := func() {
-		if options.reident {
-			write("\n")
+
+			f.buf.WriteString(strings.Repeat("  ", identLevel))
+			f.writtenInThisLine = true
 		}
 	}
-	for pos, token := range tokens {
-		if token.Value == "(" {
-			parenthesisIdented = append(parenthesisIdented, false)
-		} else if token.Value == ")" {
+
+	if f.spaceQueued != "" {
+		f.buf.WriteString(f.spaceQueued)
+		f.spaceQueued = ""
+	}
+
+	f.buf.WriteString(s)
+}
+
+func (f *formatOptionList) writeLinebreak() {
+	if f.reident {
+		f.write("\n")
+	}
+}
+
+func (f *formatOptionList) writeToken(tokens []Token, pos int) {
+	tokenType := tokens[pos].Type
+	tokenValue := tokens[pos].Value
+	if tokenType == TokenComment && f.removeComments {
+		return
+	}
+
+	if f.reident {
+		if tokenValue == "(" {
+			var shouldIdent bool
+
+			// if next keyword is SELECT, then we should ident
+			var nextKeywordToken *Token
+			for i := pos + 1; i < len(tokens); i++ {
+				if tokens[i].Type == TokenKeyword {
+					nextKeywordToken = &tokens[i]
+					break
+				}
+			}
+			if nextKeywordToken != nil && nextKeywordToken.Value == "SELECT" {
+				shouldIdent = true
+			}
+
+			f.parenthesisIdented = append(f.parenthesisIdented, shouldIdent)
+		} else if tokenValue == ")" {
 			var isParenthesisIdented bool
-			if len(parenthesisIdented) > 0 {
-				isParenthesisIdented = parenthesisIdented[len(parenthesisIdented)-1]
-				parenthesisIdented = parenthesisIdented[:len(parenthesisIdented)-1]
+			if len(f.parenthesisIdented) > 0 {
+				isParenthesisIdented = f.parenthesisIdented[len(f.parenthesisIdented)-1]
+				f.parenthesisIdented = f.parenthesisIdented[:len(f.parenthesisIdented)-1]
 			}
 			if isParenthesisIdented {
-				writeLinebreak()
+				f.writeLinebreak()
 			}
 		}
-		if token.Type == TokenName {
+
+		if tokenType == TokenWhitespace || tokenType == TokenNewline {
+			if !f.writtenInThisLine {
+				return
+			}
+			var nextToken *Token
+			if pos+1 < len(tokens) {
+				nextToken = &tokens[pos+1]
+			}
+			if nextToken != nil {
+				if nextToken.Type == TokenNewline || nextToken.Type == TokenComment {
+					return
+				}
+			}
+			if f.lastWrittenToken != nil && f.lastWrittenToken.Type != TokenWhitespace && f.lastWrittenToken.Type != TokenNewline {
+				f.lastWrittenToken = &tokens[pos]
+				f.write(" ")
+			}
+			return
+		}
+		if tokenType == TokenComment {
+			f.writeLinebreak()
+		}
+		if tokenValue == "SELECT" {
+			if len(f.parenthesisIdented) > 0 {
+				f.writeLinebreak()
+			} else {
+				// get previous non-whitespace, non-newline token
+				var prevToken *Token
+				for i := pos - 1; i >= 0; i-- {
+					if tokens[i].Type != TokenWhitespace && tokens[i].Type != TokenNewline {
+						prevToken = &tokens[i]
+						break
+					}
+				}
+				if prevToken != nil && prevToken.Value == ")" {
+					f.writeLinebreak()
+				}
+			}
+		}
+
+		if tokenType == TokenName {
 			// if it's a CTE name, the next non-whitespace tokens are always "AS", "(" and "SELECT"
 			var nextTokens []Token
 			for i := pos + 1; i < len(tokens); i++ {
@@ -82,28 +158,12 @@ func Format(tokens []Token, optionList ...FormatOption) string {
 			}
 			if len(nextTokens) == 3 {
 				if nextTokens[0].Value == "AS" && nextTokens[1].Value == "(" && nextTokens[2].Value == "SELECT" {
-					writeLinebreak()
+					f.writeLinebreak()
 				}
 			}
 		}
-		if token.Value == "SELECT" {
-			if len(parenthesisIdented) > 0 {
-				parenthesisIdented[len(parenthesisIdented)-1] = true
-				writeLinebreak()
-			} else {
-				var lastNonWhitespaceToken *Token
-				for i := pos - 1; i >= 0; i-- {
-					if tokens[i].Type != TokenWhitespace && tokens[i].Type != TokenNewline {
-						lastNonWhitespaceToken = &tokens[i]
-						break
-					}
-				}
-				if lastNonWhitespaceToken != nil && lastNonWhitespaceToken.Value == ")" {
-					writeLinebreak()
-				}
-			}
-		}
-		if token.Value == "FROM" {
+
+		if tokenValue == "FROM" {
 			// if we have more than X tokens since the select, break the line
 			var tokensSinceSelect int
 			for i := pos - 1; i >= 0; i-- {
@@ -115,16 +175,37 @@ func Format(tokens []Token, optionList ...FormatOption) string {
 				}
 			}
 
-			if tokensSinceSelect >= options.fromBreakCount {
-				writeLinebreak()
+			if tokensSinceSelect >= f.fromBreakCount {
+				f.writeLinebreak()
 			}
 		}
-		if token.Value == "WHERE" || token.Value == "ORDER BY" || token.Value == "GROUP BY" {
-			writeLinebreak()
+
+		if tokenValue == "WHERE" || tokenValue == "ORDER BY" || tokenValue == "GROUP BY" || tokenValue == "UNION ALL" || tokenValue == "LEFT OUTER JOIN" {
+			f.writeLinebreak()
 		}
 
-		write(token.Value)
+		tokenValue = strings.TrimSpace(tokenValue)
 	}
 
-	return sb.String()
+	f.lastWrittenToken = &tokens[pos]
+	f.write(tokenValue)
+}
+
+func (f *formatOptionList) formattedQuery(tokens []Token) string {
+	for i := 0; i < len(tokens); i++ {
+		f.writeToken(tokens, i)
+	}
+
+	return f.buf.String()
+
+}
+
+func Format(tokens []Token, optionList ...FormatOption) string {
+	var options formatOptionList
+
+	for _, option := range optionList {
+		option(&options)
+	}
+
+	return options.formattedQuery(tokens)
 }
--- formatter_test.go
+++ formatter_test.go
@@ -6,58 +6,83 @@ import (
 	"testing"
 )
 
-func TestFormatWithReident(t *testing.T) {
+func TestFormat(t *testing.T) {
 	tests := []struct {
 		query    string
 		expected string
+		options  []FormatOption
 	}{
 		{
 			query:    `SELECT * FROM foo`,
 			expected: `SELECT * FROM foo`,
+			options:  []FormatOption{FormatOptionReident(true), FormatOptionFromBreakCount(3)},
 		},
 		{
 			query:    `SELECT * FROM (SELECT bar FROM foo)`,
 			expected: "SELECT * FROM (\n  SELECT bar FROM foo\n)",
+			options:  []FormatOption{FormatOptionReident(true), FormatOptionFromBreakCount(3)},
 		},
 		{
 			query:    `WITH foo AS (SELECT foos, bars FROM foo_list), bar AS (SELECT * FROM foo ORDER BY bars DESC) SELECT * FROM bar`,
-			expected: "WITH \nfoo AS (\n  SELECT foos, bars \n  FROM foo_list\n), \nbar AS (\n  SELECT * FROM foo \n  ORDER BY bars DESC\n) \nSELECT * FROM bar",
+			expected: "WITH\nfoo AS (\n  SELECT foos, bars\n  FROM foo_list\n),\nbar AS (\n  SELECT * FROM foo\n  ORDER BY bars DESC\n)\nSELECT * FROM bar",
+			options:  []FormatOption{FormatOptionReident(true), FormatOptionFromBreakCount(3)},
 		},
 		{
 			query:    `WITH foo AS (SELECT foos, bars FROM foo_list WHERE foos IN (SELECT foo_id FROM ids WHERE active = true)), bar AS (SELECT * FROM foo ORDER BY bars DESC) SELECT * FROM bar`,
-			expected: "WITH \nfoo AS (\n  SELECT foos, bars \n  FROM foo_list \n  WHERE foos IN (\n    SELECT foo_id FROM ids \n    WHERE active = true\n  )\n), \nbar AS (\n  SELECT * FROM foo \n  ORDER BY bars DESC\n) \nSELECT * FROM bar",
+			expected: "WITH\nfoo AS (\n  SELECT foos, bars\n  FROM foo_list\n  WHERE foos IN (\n    SELECT foo_id FROM ids\n    WHERE active = true\n  )\n),\nbar AS (\n  SELECT * FROM foo\n  ORDER BY bars DESC\n)\nSELECT * FROM bar",
+			options:  []FormatOption{FormatOptionReident(true), FormatOptionFromBreakCount(3)},
 		},
 		{
 			query:    `SELECT foo,bar,baz,abc,xyz FROM table_name`,
-			expected: "SELECT foo,bar,baz,abc,xyz \nFROM table_name",
+			expected: "SELECT foo,bar,baz,abc,xyz\nFROM table_name",
+			options:  []FormatOption{FormatOptionReident(true), FormatOptionFromBreakCount(3)},
 		},
 		{
 			query:    "SELECT (1 + 1)\nAS result",
-			expected: "SELECT (1 + 1)\nAS result",
+			expected: "SELECT (1 + 1) AS result",
+			options:  []FormatOption{FormatOptionReident(true), FormatOptionFromBreakCount(3)},
 		},
 		{
 			query:    `SELECT (1+1), (2+2)`,
 			expected: `SELECT (1+1), (2+2)`,
+			options:  []FormatOption{FormatOptionReident(true), FormatOptionFromBreakCount(3)},
 		},
 		{
 			query:    `SELECT (1+1),(2+2)`,
 			expected: `SELECT (1+1),(2+2)`,
+			options:  []FormatOption{FormatOptionReident(true), FormatOptionFromBreakCount(3)},
+		},
+		{
+			query:    "SELECT *\n-- testing comment\nFROM bar",
+			expected: "SELECT *\n-- testing comment\nFROM bar",
+			options:  []FormatOption{FormatOptionReident(true)},
+		},
+		{
+			query:    "SELECT * -- testing comment\nFROM bar",
+			expected: "SELECT *\n-- testing comment\nFROM bar",
+			options:  []FormatOption{FormatOptionReident(true)},
+		},
+		{
+			query:    "SELECT * -- testing comment\nFROM bar",
+			expected: "SELECT * FROM bar",
+			options:  []FormatOption{FormatOptionRemoveComments(true)},
+		},
+		{
+			query:    "SELECT *\n-- testing comment\nFROM bar",
+			expected: "SELECT *\nFROM bar",
+			options:  []FormatOption{FormatOptionRemoveComments(true)},
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.query, func(t *testing.T) {
 			tokens, err := GetTokens(test.query)
-			resultReident := Format(
-				tokens,
-				FormatOptionReident(true),
-				FormatOptionFromBreakCount(3),
-			)
+			resultFormatted := Format(tokens, test.options...)
 			resultNoOptions := Format(tokens)
 
 			require.NoError(t, err, "GetTokens")
 			assert.Equal(t, test.query, resultNoOptions)
-			assert.Equal(t, test.expected, resultReident)
+			assert.Equal(t, test.expected, resultFormatted)
 		})
 	}
 }
